5aafbac9ed8a66890faad8f322a633e59749cbb4
PRL <leikange@gmail.com>
2011-04-11 21:21:04 +0200

    Ja. Har skrevet en del siden sist, ser eg. Mest på implementation siste to vekene.

--- opneRelevanteFiler.sh
+++ opneRelevanteFiler.sh
@@ -1,2 +1,2 @@
-vim -p rapport.tex introduksjon.tex detBiologiskeSystemet.tex ANN.tex theNewModel.tex implementasjon.tex metodeForSammenligning.tex resultat.tex spikingANN.tex synPlast.tex tid.tex bibliografi.bib;
+vim -p rapport.tex introduksjon.tex detBiologiskeSystemet.tex theNewModel.tex ANN.tex implementasjon.tex metodeForSammenligning.tex resultat.tex spikingANN.tex synPlast.tex tid.tex bibliografi.bib;
 
