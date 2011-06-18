7470a040d2336d50f9f38728bdb24c626c859bb4
Per Leikanger <leikange@gmail.com>
2011-06-19 01:45:06 +0200

    Har lagt til appendix om syn.p. (fra ovskrevet fra NEVR3001-rapport)

--- opneRelevanteFiler.sh
+++ opneRelevanteFiler.sh
@@ -1,2 +1,2 @@
-vim -p rapport.tex introduksjon.tex ANN.tex detBiologiskeSystemet.tex theNewModel.tex design.tex implementasjon_SANN.tex implementasjon_KANN.tex metodeForSammenligning.tex resultat.tex spikingANN.tex bibliografi.bib;
+vim -p rapport.tex introduksjon.tex ANN.tex detBiologiskeSystemet.tex theNewModel.tex design.tex implementasjon_SANN.tex implementasjon_KANN.tex metodeForSammenligning.tex resultat.tex spikingANN.tex appendixSynPlast.tex bibliografi.bib
 
