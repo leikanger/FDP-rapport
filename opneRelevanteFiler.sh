e231e3c0b96bf1cfabcd935e8d1d52c8d1aa4c5d
Per Leikanger <leikange@gmail.com>
2011-06-22 22:56:14 +0200

    Endra litt på oppsettet.. (to kap. til slutt: eit comparisonAndResults og eit discussionAndConclusion

--- opneRelevanteFiler.sh
+++ opneRelevanteFiler.sh
@@ -1,2 +1,2 @@
-vim -p rapport.tex introduksjon.tex ANN.tex detBiologiskeSystemet.tex theNewModel.tex design.tex implementasjon_SANN.tex implementasjon_KANN.tex metodeForSammenligning.tex appendixSynPlast.tex bibliografi.bib
+vim -p rapport.tex introduksjon.tex ANN.tex detBiologiskeSystemet.tex theNewModel.tex design.tex implementasjon_SANN.tex implementasjon_KANN.tex comparisonAndResults.tex discussionAndConclusion.tex appendixSynPlast.tex bibliografi.bib
 
